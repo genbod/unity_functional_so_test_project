25adc6b110f9d2214c065aa1a6af2b753e9d887d
Ben Godard <bgodard@outlook.com>
2019-05-30 10:13:58 -0700

    added git

---
+++ Assets/_Scripts/ReadOnlyStringUpdateTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static F;
+
+public class ReadOnlyStringUpdateTest : MonoBehaviour
+{
+    public StringVariable stringVariable;
+    public TextReplacer textReplacer;
+
+    public void ChangeStringVariable()
+    {
+        //stringVariable.SetValue("This is a new value set from Code");
+        stringVariable = StringVariable.CreateAsReadOnly(new StringVariable() { Value = Some("NEW STRING VARIABLE!") });
+        textReplacer.GetValueToString = stringVariable.GetValueToString;
+    }
+
+    public void ChangeStringValue()
+    {
+        stringVariable.SetValue("ERROR! NEW STRING VALUE!");
+    }
+}
